5fd428f455df364283f6235bf257156351ee07f1
AlexBedwell <abedwell20@gmail.com>
2024-01-17 00:00:35 -0800

    nav updated

--- main.js
+++ main.js
@@ -37,8 +37,20 @@ $(document).ready(function(){
 }); 
 
 
-function mHideHeader(){
-    while ($(window).width() < 1280) {
-
+function dropMenu() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
     }
-}
+  }
